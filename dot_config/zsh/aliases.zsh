c95587c22e42eccfd0065cf5a40e3613d83f2e6a
Donato Aguirre <20030075+donatoaguirre24@users.noreply.github.com>
2025-10-02 15:52:17 -0300

    Update ZSH config

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,23 +1,24 @@
 #!/usr/bin/env zsh
 
 # General
-alias brewery="brew update && brew upgrade && brew upgrade --cask && brew cleanup"
-alias c="clear"
+alias brewery='brew update && brew upgrade && brew upgrade --cask && brew cleanup'
+alias c='clear'
 alias dotfiles="cursor $(chezmoi source-path)"
 alias reload="source $ZSHRC"
-alias show_packages_size="du -sh ./node_modules/* | sort -nr | rg '\dM.*'"
+alias show_packages_size='du -sh ./node_modules/* | sort -nr | rg "\dM.*"'
 alias show_path="tr ':' '\n' <<< \"$PATH\""
-alias speedtest="networkQuality"
-alias update_all="zap update all && chezmoi upgrade && mise self-update --yes && brewery"
+alias show_path_expanded='print -l $path' # Like show_path, but with ZSH hooks applied
+alias speedtest='networkQuality'
+alias update_all='zap update all && chezmoi upgrade && mise self-update --yes && brewery'
 
 # Mobile Development
-alias boot_emulator="emulator @Pixel_8_API_34 -no-boot-anim -no-snapshot -dns-server 8.8.8.8"
-alias boot_simulator="xcrun simctl boot E87FC0D4-12FB-494E-99D8-2A7F4D7CB91C && open -a Simulator"
-alias cdd="trash ~/Library/Developer/Xcode/DerivedData/*"
+alias boot_emulator='emulator @Pixel_8_API_34 -no-boot-anim -no-snapshot -dns-server 8.8.8.8'
+alias boot_simulator='xcrun simctl boot E87FC0D4-12FB-494E-99D8-2A7F4D7CB91C && open -a Simulator'
+alias cdd='trash ~/Library/Developer/Xcode/DerivedData/*'
 
 # FZF
 if [[ -v commands[fd] ]]; then
-  alias find="fd --type f --hidden --follow --exclude .git"
+  alias find='fd --type f --hidden --follow --exclude .git'
 fi
 
 # LSD
@@ -30,7 +31,7 @@ fi
 
 # Trash
 if [[ -v commands[trash] ]]; then
-  alias rm="trash"
+  alias rm='trash'
 fi
 
 # Zoxide
@@ -41,10 +42,10 @@ fi
 # Docker
 if [[ -v commands[docker] ]]; then
   alias dc=dc-fn # defined in functions.zsh
-  alias dcu="docker compose up -d"
-  alias dcd="docker compose down --remove-orphans"
+  alias dcu='docker compose up -d'
+  alias dcd='docker compose down --remove-orphans'
   alias dcr=dcr-fn # defined in functions.zsh
   alias dex=dex-fn # defined in functions.zsh
-  alias dps="docker ps"
-  alias dpsa="docker ps -a"
+  alias dps='docker ps'
+  alias dpsa='docker ps -a'
 fi
