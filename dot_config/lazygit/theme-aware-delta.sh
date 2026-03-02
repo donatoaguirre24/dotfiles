017d1b0584d6f043230e3842029947c386b0d613
Donato Aguirre <20030075+donatoaguirre24@users.noreply.github.com>
2026-03-01 23:08:05 -0300

    Fix scrolling in Delta and Dark mode in Lazygit

---
+++ dot_config/lazygit/theme-aware-delta.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check if dark mode is enabled (make sure to chmod +x this file)
+if defaults read -g AppleInterfaceStyle &>/dev/null; then
+  exec delta --dark "$@"
+else
+  exec delta --light "$@"
+fi
